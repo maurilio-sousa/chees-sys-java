9934786b7c6470842a46e2734a139f4c74eee87c
Maurilio-Sousa <mauriliolilo960@gmail.com>
2024-08-12 17:17:19 -0300

    implementing possible moves of king

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,9 +45,12 @@ public class Program {
 			
 			UI.printBoard(chessMatch.getPieces());
 			System.out.println();
-			System.out.println("Sourse: ");
+			System.out.print("Sourse: ");
 			ChessPosition source = UI.readChessPosition(sc);
 			
+			boolean [][] possibleMoves = chessMatch.possibleMoves(source);
+			UI.clearScreen();
+			UI.printBoard(chessMatch.getPieces(), possibleMoves);
 			System.out.println();
 			System.out.print("Target: ");
 			ChessPosition target = UI.readChessPosition(sc);
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,15 +1,16 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece{
+public class King extends ChessPiece {
 
 	public King(Board board, Color color) { // the same method for rook but now is king
 		super(board, color);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "K";
@@ -17,7 +18,63 @@ public class King extends ChessPiece{
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+
+		// above | position above King
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) { // if position above there no have a piece then:
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) { // if position below there no have a piece then:
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) { // if position left there no have a piece then:
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) { // if position right there no have a piece then:
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// diagonal up left
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) { 
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// diagonal up right
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) { 
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// diagonal below left
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) { 
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
+		// diagonal below right
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) { 
+			mat[p.getRow()][p.getColumn()] = true; // move
+		}
+
 		return mat;
 	}
+
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position); // get position the piece p in my board
+		return p == null || p.getColor() != getColor(); 
+	}
+
 }
